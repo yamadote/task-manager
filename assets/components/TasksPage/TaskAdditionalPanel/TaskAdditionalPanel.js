fbbba5779ed5c6d014f52b019f77e9f4c9a865e8
yamadote <yamadote@gmail.com>
2021-05-29 19:00:55 +0300

    refactoring

--- assets/components/TasksPage/TaskAdditionalPanel/TaskAdditionalPanel.js
+++ assets/components/TasksPage/TaskAdditionalPanel/TaskAdditionalPanel.js
@@ -3,20 +3,22 @@ import React, {useState} from 'react';
 import './TaskAdditionalPanel.scss';
 
 const TaskAdditionalPanel = ({task, events}) => {
-    const [isLinkChanging, setLinkChanging] = useState(false);
-    const linkClassName = isLinkChanging ? "changing" : "";
-    const onLinkChange = event => events.updateTaskLink(task.id, event.target.value, setLinkChanging);
-
-    const onNewTaskButton = () => {events.createNewTask(task)};
-    const onRemoveTaskButton= () => {events.removeTask(task)};
-    return (
-        <div className="mb-3">
+    const renderLinkField = () => {
+        const [isLinkChanging, setLinkChanging] = useState(false);
+        const linkClassName = isLinkChanging ? "changing" : "";
+        const onLinkChange = event => events.updateTaskLink(task.id, event.target.value, setLinkChanging);
+        return (
             <div className="field link">
                 <span>Link:</span>
                 <input type="text" value={task.link ?? ''} onChange={onLinkChange} className={linkClassName}/>
             </div>
-            <button onClick={onNewTaskButton} className='btn btn-sm btn-secondary'>New Task</button>
-            <button onClick={onRemoveTaskButton} className='btn btn-sm btn-danger'>Remove</button>
+        );
+    }
+    return (
+        <div className="mb-3">
+            { renderLinkField() }
+            <button onClick={() => {events.createNewTask(task)}} className='btn btn-sm btn-secondary'>New Task</button>
+            <button onClick={() => {events.removeTask(task)}} className='btn btn-sm btn-danger'>Remove</button>
         </div>
     );
 }
--- assets/components/TasksPage/TaskTitle/TaskTitle.js
+++ assets/components/TasksPage/TaskTitle/TaskTitle.js
@@ -7,37 +7,37 @@ const TaskTitle = ({task, children, events}) => {
     const titleClassName = "title" + (isTitleChanging ? " changing" : "");
     const onTitleChange = event => events.updateTaskTitle(task.id, event.target.value, setTitleChanging);
 
-    const displayChildrenViewButton = () => {
-        const onChildrenViewButton = () => events.updateTaskChildrenViewSetting(task.id);
+    const renderChildrenViewButton = () => {
+        const onButtonClick = () => events.updateTaskChildrenViewSetting(task.id);
         const iconClassName = task.isChildrenOpen ? "oi oi-chevron-top" : "oi oi-chevron-bottom";
         return (
-            <button onClick={onChildrenViewButton} className='title-button mr-1'>
+            <button onClick={onButtonClick} className='title-button mr-1'>
                 <span className={iconClassName}/>
             </button>
         );
     }
-    const displayLink = () => {
+    const renderLink = () => {
         return (
             <a href={task.link} target="_blank" className="title-link">
                 <span className="oi oi-account-login"/>
             </a>
         );
     }
-    const displayAdditionalPanel = () => {
-        const onAdditionalPanelViewButton = () => events.updateTaskAdditionalPanelViewSetting(task.id)
+    const renderAdditionalPanelViewButton = () => {
+        const onButtonClick = () => events.updateTaskAdditionalPanelViewSetting(task.id)
         const iconClassName = task.isAdditionalPanelOpen ? "oi oi-chevron-top" : "oi oi-chevron-bottom";
         return (
-            <button onClick={onAdditionalPanelViewButton} className='title-button'>
+            <button onClick={onButtonClick} className='title-button'>
                 <span className={iconClassName}/>
             </button>
         );
     }
     return (
         <div className="title-group">
-            { children.length > 0 ? displayChildrenViewButton() : null }
+            { children.length > 0 ? renderChildrenViewButton() : null }
             <input className={titleClassName} type="text" value={task.title} onChange={onTitleChange} />
-            { task.link ? displayLink() : null }
-            { displayAdditionalPanel() }
+            { task.link ? renderLink() : null }
+            { renderAdditionalPanelViewButton() }
         </div>
     )
 }
