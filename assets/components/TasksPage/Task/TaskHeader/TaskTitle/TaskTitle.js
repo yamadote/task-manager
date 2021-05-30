88c319a961c70420cf421344e1ec41bd7d18d483
yamadote <yamadote@gmail.com>
2021-05-30 21:46:33 +0300

    adding input focus styles

--- assets/components/TasksPage/Task/TaskHeader/TaskTitle/TaskTitle.js
+++ assets/components/TasksPage/Task/TaskHeader/TaskTitle/TaskTitle.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 const TaskTitle = ({task, events}) => {
     const [isTitleChanging, setTitleChanging] = useState(false);
-    const titleClassName = "title" + (isTitleChanging ? " changing" : "");
+    const titleClassName = "title mr-1 " + (isTitleChanging ? " changing" : "");
     const onTitleChange = event => events.updateTaskTitle(task.id, event.target.value, setTitleChanging);
     return <input className={titleClassName} type="text" value={task.title} onChange={onTitleChange} />
 }
