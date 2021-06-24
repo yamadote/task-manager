0ac578e30d7369cde71887f2218714c51a9e157b
yamadote <yamadote@gmail.com>
2021-06-24 23:37:58 +0300

    refactoring

--- assets/components/TasksPage/Footer/Footer.js
+++ assets/components/TasksPage/Footer/Footer.js
@@ -1,27 +1,10 @@
 
 import React from 'react';
-import './Footer.scss';
-
+import TasksAmount from "./TasksAmount/TasksAmount";
 
 const Footer = ({tasks, root, nested}) => {
-    if (!tasks) {
-        return null;
-    }
-    const count = (tasks, parent) => {
-        let amount = 0;
-        tasks.forEach(task => {
-            if (task.parent === parent) {
-                amount += count(tasks, task.id) + 1;
-            }
-        });
-        return amount;
-    }
-    let amount = root && nested ? count(tasks, root.id) : tasks.length;
-    if (amount === 0) {
-        return null;
-    }
     return (
-        <div className='task-amount'>Tasks Amount: {amount}</div>
+        <TasksAmount tasks={tasks} root={root} nested={nested}/>
     );
 }
 
---
+++ assets/components/TasksPage/Footer/TasksAmount/TasksAmount.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import './TasksAmount.scss';
+
+const TasksAmount = ({tasks, root, nested}) => {
+    if (!tasks) {
+        return null;
+    }
+    const count = (tasks, parent) => {
+        let amount = 0;
+        tasks.forEach(task => {
+            if (task.parent === parent) {
+                amount += count(tasks, task.id) + 1;
+            }
+        });
+        return amount;
+    }
+    let amount = root && nested ? count(tasks, root.id) : tasks.length;
+    if (amount === 0) {
+        return null;
+    }
+    return (
+        <div className='task-amount'>Tasks Amount: {amount}</div>
+    );
+}
+
+export default TasksAmount;
