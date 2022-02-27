987ae0bd4e80c35882084a68c871097b01fee7ef
yamadote <yamadote@gmail.com>
2022-02-27 20:15:16 +0200

    change task link icon

--- assets/components/TasksPage/Task/TaskHeader/TaskLink/TaskLink.js
+++ assets/components/TasksPage/Task/TaskHeader/TaskLink/TaskLink.js
@@ -4,7 +4,7 @@ import React from 'react';
 const TaskLink = ({link}) => {
     return (
         <a href={link} target="_blank" className="title-link">
-            <span className="oi oi-account-login"/>
+            <span className="oi oi-external-link"/>
         </a>
     )
 }
