be430089b9ff644288f3170433dba63b3d56b5cb
yamadote <yamadote@gmail.com>
2022-03-03 00:26:35 +0200

    fix task title panel & parent title

--- assets/components/TasksPage/TaskPanelHeading/TaskPanelHeading.js
+++ assets/components/TasksPage/TaskPanelHeading/TaskPanelHeading.js
@@ -8,16 +8,18 @@ const TaskPanelHeading = ({title, root, events}) => {
     return (
         <div className="panel-heading">
             <h3 className="panel-title">
-                <button className="toggle-sidebar btn-to-link"><span className="fa fa-angle-double-left" data-toggle="offcanvas" title="Maximize Panel" /></button>
-                {title}
-                {/*{root?.title}*/}
-                {/*<Link to={Helper.getTaskPageUrl(root?.parent)} className="nav-link">Back</Link>*/}
+                <button className="toggle-sidebar btn-to-link">
+                    <span className="fa fa-angle-double-left" data-toggle="offcanvas" title="Maximize Panel" />
+                </button>
                 <button className="btn btn-default" onClick={() => events.createNewTask(root?.id)}>
                     <span className="oi oi-plus"/>
                 </button>
                 <button className="btn btn-default" onClick={() => events.reload()}>
                     <span className="oi oi-reload"/>
                 </button>
+                {title}
+                {root ? <Link className="btn btn-default" to={Helper.getTaskPageUrl(root?.parent)}><span className="oi oi-share-boxed"/></Link> : null}
+                {root ? <span className="root-title">{root.title}</span> : ''}
             </h3>
         </div>
     );
