e2ba71db34bb359778baaf53317adb18b868da22
yamadote <yamadote@gmail.com>
2022-03-02 12:44:48 +0200

    add basic template

--- assets/components/TasksPage/Task/TaskHeader/TaskChildrenViewButton/TaskChildrenViewButton.js
+++ assets/components/TasksPage/Task/TaskHeader/TaskChildrenViewButton/TaskChildrenViewButton.js
@@ -5,7 +5,7 @@ const TaskChildrenViewButton = ({task, events}) => {
     const onButtonClick = () => events.updateTaskChildrenViewSetting(task.id, !task.isChildrenOpen);
     const iconClassName = task.isChildrenOpen ? "oi oi-chevron-top" : "oi oi-chevron-bottom";
     return (
-        <button onClick={onButtonClick} className='title-button mr-1'>
+        <button onClick={onButtonClick} className='title-button'>
             <span className={iconClassName}/>
         </button>
     );
