012d780690ed4cc8a4edad01974b5f5dc6a38b6d
yamadote <yamadote@gmail.com>
2022-04-25 17:17:36 +0300

    Implement event drag

--- assets/components/TasksPage/TasksCalendar/TasksCalendar.js
+++ assets/components/TasksPage/TasksCalendar/TasksCalendar.js
@@ -5,21 +5,30 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"
 import './TasksCalendar.scss';
 
-const TasksCalendar = ({tasks}) => {
+const TasksCalendar = ({tasks, events}) => {
     const reminders = tasks ? tasks.filter(task => !task.isHidden && task.reminder) : [];
-    const events = reminders.map(task => {
+    const calendarEvents = reminders.map(task => {
         const date = new Date(task.reminder * 1000);
         return {
+            id: task.id,
             title: task.title,
             date: date.toISOString()
         };
     });
+    const eventDrop = (info) => {
+        const taskId = parseInt(info.event._def.publicId);
+        const task = tasks.find(task => task.id === taskId);
+        const reminder = info.event.start.getTime() / 1000;
+        events.updateTaskReminder(task, reminder)
+    };
     return (
         <div className="calendar">
             <FullCalendar
                 plugins={[ dayGridPlugin, interactionPlugin ]}
                 initialView="dayGridWeek"
-                events={events}
+                events={calendarEvents}
+                eventDrop={eventDrop}
+                editable={true}
                 contentHeight={118}
                 firstDay={1}
             />
