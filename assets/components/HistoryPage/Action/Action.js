44e13dd8cc0adc969ae0742c3153a80f1a7d832d
yamadote <yamadote@gmail.com>
2022-05-17 21:04:24 +0300

    Add bbcode render to action message (History page)

--- assets/components/HistoryPage/Action/Action.js
+++ assets/components/HistoryPage/Action/Action.js
@@ -1,6 +1,42 @@
 
 import React from 'react';
 import moment from "moment";
+import parser, {Tag} from 'bbcode-to-react';
+
+class TitleTag extends Tag {
+    toReact() {
+        const title = this.getContent(true);
+        return <span>"{title}"</span>;
+    }
+}
+
+class LinkTag extends Tag {
+    toReact() {
+        const link = this.getContent(true);
+        return <a href={link}>{link}</a>;
+    }
+}
+
+class ReminderTag extends Tag {
+    toReact() {
+        const timestamp = this.getContent(true);
+        const time = moment.unix(timestamp).format('YYYY/MM/DD HH:mm');
+        return <b>{time}</b>;
+    }
+}
+
+class StatusTag extends Tag {
+    toReact() {
+        const title = this.getContent(true);
+        const slug = this.params.slug;
+        return <span className={"status-" + slug}>"{title}"</span>;
+    }
+}
+
+parser.registerTag('title', TitleTag);
+parser.registerTag('link', LinkTag);
+parser.registerTag('reminder', ReminderTag);
+parser.registerTag('status', StatusTag);
 
 const Action = ({action}) => {
     let className = '';
@@ -14,7 +50,7 @@ const Action = ({action}) => {
     return (
         <tr className={className}>
             <td className="column time-column">{time}</td>
-            <td className="column message-column">{action.message}</td>
+            <td className="column message-column">{parser.toReact(action.message)}</td>
             <td className="column task-column"><div className="title">{action.task}</div></td>
         </tr>
     );
